8083d348e1cf25b3398b0fd84c95a9a71edc9e11
Hae Won Park <haewon@mit.edu>
2019-11-19 17:34:35 -0500

    fixed names

--- filenames_handler.py
+++ filenames_handler.py
@@ -17,10 +17,13 @@ def change_format_audios():
 
 			for student_audio_file in os.listdir(path_to_read_group):
 				if student_audio_file.endswith(".mp3"): #just check if valid audio
-					print("Previous:", student_audio_file)
+					#print("Previous:", student_audio_file)
 					good_format_name = format_name(student_audio_file)
-					print("Changed:",good_format_name)
-					os.rename("{}/{}".format(path_to_read_group,student_audio_file), "{}/{}".format(path_to_read_group, good_format_name))
+					#print("Changed:",good_format_name)
+					if student_audio_file != good_format_name:
+						print("Formatting filename form {} to {}".format(student_audio_file, good_format_name))
+						os.rename("{}/{}".format(path_to_read_group,student_audio_file), "{}/{}".format(path_to_read_group, good_format_name))
+						
 			"""
 			only_audio_files = "{}/*.mp3".format(path_to_read_group)
 			for student_audio_file in glob.glob(only_audio_files):
